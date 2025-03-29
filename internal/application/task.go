66e0bd4a691e9943cf40cce4ec74996fae5ba85c
zhanghua.02 <zhanghua.02@bytedance.com>
2025-03-29 10:02:04 +0800

    add it

--- internal/application/task.go
+++ internal/application/task.go
@@ -2,9 +2,25 @@ package application
 
 import (
 	"context"
+	"notify/internal/domain/task"
 )
 
-type App struct {
+type Appalition interface {
+	CreateTask(ctx context.Context, userId int64, task *task.Task) error
+}
+
+type AppalitionImpl struct {
 	ctx         context.Context
-	taskService TaskService
+	taskService task.TaskService
+}
+
+func NewAppalition(ctx context.Context, taskService task.TaskService) Appalition {
+	return &AppalitionImpl{
+		ctx:         ctx,
+		taskService: taskService,
+	}
+}
+
+func (a *AppalitionImpl) CreateTask(ctx context.Context, userId int64, task *task.Task) error {
+	return a.taskService.CreateTask(a.ctx, userId, task)
 }
